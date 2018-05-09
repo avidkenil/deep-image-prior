1b2f44d1fbd111e4d1a0745e863f1d8ede1ecab0
Kenil Tanna <avidkenil@gmail.com>
2018-05-08 22:20:00 -0400

    added file to load all modules on prince

--- load_modules_prince.sh
+++ load_modules_prince.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
-module load pytorch/python3.6/0.3.0_4
+module load pytorch/python2.7/0.3.0_4
 module load torchvision/0.1.8
 module load scikit-learn/intel/0.18.1
 module load scipy/intel/0.19.1
-module load numpy/python3.6/intel/1.14.0
+module load numpy/python2.7/intel/1.14.0
